0b23816e3c2d9c74bdf027d82c8f88a9ab9382b4
Leonardo Garcia barbero <lgarcia-@z2r1p4.le-101.fr>
2018-10-25 21:14:18 +0200

    Hay que resolver todavía problemas con el chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,50 +1,75 @@
 $(function(){
 
     function escapeHtml(text) {
-        var map = {
-          '&': '&amp;',
-          '<': '&lt;',
-          '>': '&gt;',
-          '"': '&quot;',
-          "'": '&#039;'
-        };
-      
-        return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+        if (text)
+        {
+            var map = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#039;'
+            };
+        
+            return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+        }
+        else
+            return (false);
+        
       }
 
 // var login = $('#desplegable').text(); ya esta en el header
-    var userImg = escapeHtml($('#userImg').attr('src'));
+    var userImg = escapeHtml($('.userImg').attr('src'));
     var destinoImg = escapeHtml($('#imgAvatar').attr('src'));
 
-    $("#addClass").click(function () {
-        $('#qnimate').addClass('popup-box-on');
-    });
+    // $(".addClass").click(function () {
+    //     $('#qnimate').addClass('popup-box-on');
+    // });
         
-    $("#removeClass").click(function () {
-        $('#qnimate').removeClass('popup-box-on');
-    });
+    // $(".removeClass").click(function () {
+    //     $('#qnimate').removeClass('popup-box-on');
+    // });
 
     $(".addClass").click(function () {
         var midiv = $(this).closest('div');
         $(midiv).prev().addClass('popup-box-on');
     });
-        
+
     $(".removeClass").click(function () {
         $(this).closest('.popup-box').removeClass('popup-box-on');
     });
 
-    $("#status_message").on("keydown", function(e) {
+    // $("#status_message").on("keydown", function(e) {
+    //     if (e.which == 13){
+    //         var text = $(this).val();
+    //         if (text !== ""){
+    //             insertChat();
+    //             $(this).val('');
+    //         }
+    //     }
+    // });
+    // $("#enviar").click(function(){
+    //     insertChat();
+    //     $('#status_message').val('');
+    // });
+
+    $("textarea").keydown(function(e){
         if (e.which == 13){
+            var user = $(this).closest('.blockChat').attr('id');
             var text = $(this).val();
             if (text !== ""){
-                insertChat();
+                text = escapeHtml(text);
+                insertChat(text, user);              
                 $(this).val('');
             }
         }
     });
-    $("#enviar").click(function(){
-        insertChat();
-        $('#status_message').val('');
+    $(".enviar").click(function(){
+        var user = $(this).closest('.blockChat').attr('id');
+        var text = $(this).prev().val();
+        text = escapeHtml(text);
+        insertChat(text, user);
+        $(this).prev().val('');
     });
 
     function formatAMPM(date) {
@@ -58,10 +83,10 @@ $(function(){
         return strTime;
     }
 
-    function insertChat()
+    function insertChat(text, userDst)
     {
-        var message = escapeHtml($.trim($('#status_message').val()));
-        var destinatario = escapeHtml($('#login').text());
+        var message = $.trim(text);
+        var destinatario = userDst;
         login = escapeHtml(login);
         if (message != "")
         {
@@ -93,52 +118,52 @@ $(function(){
     {
         if (data.msg != "")
         {
-
-            if (!$('#' + data.user).length)
-          {
-            var d = new Date(); 
-            var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-            var date = d.toLocaleDateString('fr-FR', options);
-            var elemento = 
-            `
-              <div id="` + data.user + `" class="blockChat">
-                    <div class="popup-box chat-popup popup-box-on">
-                        <div class="popup-head">
-                            <div class="popup-head-left pull-left">
-                                <img class="dstImg" src="` + data.userImg + `" alt="` + data.user + `"> ` + data.user +
-                                ` - ` + data.dst + ` <img class="userImg" src="` + data.dstImg + `" alt="` + data.dst + `">` +
-                            ` </div>
-                            <div class="popup-head-right pull-right">
-                                <button data-widget="remove"  class="removeClass chat-header-button pull-right" type="button">
-                                    <i class="fa fa-power-off"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="popup-messages">
-                            <div class="direct-chat-messages" id="mensajes">
-                                <div class="chat-box-single-line">
-                                    <abbr class="timestamp">` + date + `</abbr>
+            var dest = escapeHtml($('#login').text());
+            if (!$('#' + data.user).length && dest != data.user)
+            {
+                var d = new Date(); 
+                var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+                var date = d.toLocaleDateString('fr-FR', options);
+                var elemento = 
+                `
+                <div id="` + data.user + `" class="blockChat">
+                        <div class="popup-box chat-popup popup-box-on">
+                            <div class="popup-head">
+                                <div class="popup-head-left pull-left">
+                                    <img class="dstImg" src="` + data.userImg + `" alt="` + data.user + `"> ` + data.user +
+                                    ` - ` + data.dst + ` <img class="userImg" src="` + data.dstImg + `" alt="` + data.dst + `">` +
+                                ` </div>
+                                <div class="popup-head-right pull-right">
+                                    <button data-widget="remove"  class="removeClass chat-header-button pull-right" type="button">
+                                        <i class="fa fa-power-off"></i>
+                                    </button>
                                 </div>
                             </div>
+                            <div class="popup-messages">
+                                <div class="direct-chat-messages" id="mensajes">
+                                    <div class="chat-box-single-line">
+                                        <abbr class="timestamp">` + date + `</abbr>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="popup-messages-footer">
+                                <textarea placeholder="Type a message..." rows="10" cols="40" name="message"></textarea>
+                                <!-- <div class="btn-footer"> -->
+                                <button class="enviar btn-outline-info rounded-circle boton">
+                                    <i class="fa fa-share"></i>
+                                </button>
+                            </div>
+                            <!-- <div class="btn-footer"></div> -->
                         </div>
-                        <div class="popup-messages-footer">
-                            <textarea class="input" placeholder="Type a message..." rows="10" cols="40" name="message"></textarea>
-                            <!-- <div class="btn-footer"> -->
-                            <button class="enviar btn-outline-info rounded-circle boton">
-                                <i class="fa fa-share"></i>
-                            </button>
+                        <div class="round hollow text-center botonChat">
+                            <a class="addClass"><span class="fa fa-comment"></span>` + data.user + `</a>
                         </div>
-                        <!-- <div class="btn-footer"></div> -->
-                    </div>
-                    <div class="round hollow text-center botonChat">
-                        <a class="addClass"><span class="fa fa-comment"></span>` + data.user + `</a>
-                    </div>
-                </div>`;
+                    </div>`;
 
-            $('.contenedorChat').append(elemento);
-            var parteChat = $('#' + data.user + ' .chat-popup .popup-messages');
-            parteChat.animate({ scrollTop: parteChat.prop('scrollHeight')}, 'slow');
-          }
+                $('.contenedorChat').append(elemento);
+                var parteChat = $('#' + data.user + ' .chat-popup .popup-messages');
+                parteChat.animate({ scrollTop: parteChat.prop('scrollHeight')}, 'slow');
+            }
             data['msg'] = escapeHtml(data.msg);
             hora = formatAMPM(new Date());
             var elemento = `
@@ -158,9 +183,29 @@ $(function(){
                 <div class="direct-chat-info clearfix"></div>
                 <!-- /.direct-chat-text -->
             </div>`;
-            $('.direct-chat-messages').append(elemento);
+            $('#' + data.user + ' .popup-box .popup-messages .direct-chat-messages').append(elemento);
+            //$('.direct-chat-messages').append(elemento);
             $(".popup-messages").animate({ scrollTop: $(".popup-messages").prop('scrollHeight')}, 'slow');
         }
+        // $("textarea").keydown(function(e){
+        //     if (e.which == 13){
+        //         alert('Intro pulsado');
+        //         var user = $(this).closest('.blockChat').attr('id');
+        //         var text = $(this).val();
+        //         if (text !== ""){
+        //             text = escapeHtml(text);
+        //             insertChat(text, user);              
+        //             $(this).val('');
+        //         }
+        //     }
+        // });
+        // $(".enviar").click(function(){
+        //     var user = $(this).closest('.blockChat').attr('id');
+        //     var text = $(this).prev().val();
+        //     text = escapeHtml(text);
+        //     insertChat(text, user);
+        //     $(this).prev().val('');
+        // });
     });
 
     socket.on('newOnline', (newUser) =>
